157fdd57a6d49dfb1ae96a80700c257292137ed3
sanchez <145916188+ravzasanchez@users.noreply.github.com>
2023-11-20 11:36:30 +0300

    Add files via upload

---
+++ form30/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace form30
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbİsimler = new System.Windows.Forms.ListBox();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.txtArama = new System.Windows.Forms.TextBox();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbİsimler
+            // 
+            this.lbİsimler.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbİsimler.FormattingEnabled = true;
+            this.lbİsimler.ItemHeight = 25;
+            this.lbİsimler.Items.AddRange(new object[] {
+            "yezda ",
+            "nursevım",
+            "ravza",
+            "ada senturk",
+            "durmus",
+            "oscar",
+            "fatma",
+            "mehmet",
+            "bulut",
+            " amca",
+            "neşe annem",
+            "HALLEY",
+            "mücahit"});
+            this.lbİsimler.Location = new System.Drawing.Point(21, 114);
+            this.lbİsimler.Name = "lbİsimler";
+            this.lbİsimler.Size = new System.Drawing.Size(162, 254);
+            this.lbİsimler.TabIndex = 0;
+            // 
+            // btnAra
+            // 
+            this.btnAra.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAra.Location = new System.Drawing.Point(192, 39);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(79, 44);
+            this.btnAra.TabIndex = 1;
+            this.btnAra.Text = "ARA";
+            this.btnAra.UseVisualStyleBackColor = true;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // txtArama
+            // 
+            this.txtArama.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtArama.Location = new System.Drawing.Point(21, 39);
+            this.txtArama.Multiline = true;
+            this.txtArama.Name = "txtArama";
+            this.txtArama.Size = new System.Drawing.Size(162, 41);
+            this.txtArama.TabIndex = 2;
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSonuc.Location = new System.Drawing.Point(212, 114);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(24, 25);
+            this.lblSonuc.TabIndex = 3;
+            this.lblSonuc.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 388);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.txtArama);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.lbİsimler);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbİsimler;
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.TextBox txtArama;
+        private System.Windows.Forms.Label lblSonuc;
+    }
+}
+
---
+++ form30/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form30
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            string aranan = txtArama.Text;
+            int sayac = 0;
+            for (int i = 0; i < lbİsimler.Items.Count; i++)
+            {
+                if (aranan == lbİsimler.Items[i].ToString())
+                {
+                    sayac++;
+                }
+            }
+            if (sayac == 0)
+            {
+                lblSonuc.Text = "Kişi Bulunamadı";
+            }
+            else
+            {
+                lblSonuc.Text = sayac + "  Kişi Bulundu";
+            }
+        }
+    }
+}
